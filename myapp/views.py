8f50767fbe4aaf0207c4bac8bc6b87de599cc88d
Edward-Dao <128796396+Edward-Dao@users.noreply.github.com>
2023-07-25 15:16:49 +0700

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,59 +1,62 @@
 from django.shortcuts import render
 from .models import *
 
+def dashboard(request):
+    return render(request, 'dashboard.html', {'mes': 'anything'})
+
 # ------------------------------------------- SELECT --------------------------------------------
 
 def bc_congno(request):
     rec = Baocaocongno.objects.all()
-    return render(request, )
+    return render(request, 'dashboard.html')
 
 def bc_doanhso(request):
     rec = Baocaodoanhso.objects.all()
-    return render(request, )
+    return render(request, 'dashboard.html')
 
 def ct_pnh(request):
     rec = CtPnh.objects.all()
-    return render(request, )
+    return render(request, 'dashboard.html')
 
 def ct_pxh(request):
     rec = CtPxh.objects.all()
-    return render(request, )
+    return render(request, 'dashboard.html')
 
 def dvt(request):
     rec = Dvt.objects.all()
-    return render(request, )
+    return render(request, 'dashboard.html')
 
 def daily(request):
     rec = Daily.objects.all()
-    return render(request, )
+    return render(request, 'dashboard.html')
 
 def loaidaily(request):
     rec = Loaidaily.objects.all()
-    return render(request, )
+    return render(request, 'dashboard.html')
 
 def mathang(request):
     rec = Mathang.objects.all()
-    return render(request, )
+    return render(request, 'dashboard.html')
 
 def phieunhaphang(request):
     rec = Phieunhaphang.objects.all()
-    return render(request, )
+    return render(request, 'dashboard.html')
 
 def phieuthutien(request):
     rec = Phieuthutien.objects.all()
-    return render(request, )
+    return render(request, 'dashboard.html')
 
 def phieuxuathang(request):
     rec = Phieuxuathang.objects.all()
-    return render(request, )
+    return render(request, 'dashboard.html')
 
 def quan(request):
     rec = Quan.objects.all()
-    return render(request, )
+    return render(request, 'dashboard.html')
 
 def thamso(request):
     rec = Thamso.objects.all()
-    return render(request, )
+    return render(request, 'dashboard.html')
 
 # ------------------------------------------- INSERT --------------------------------------------
 
