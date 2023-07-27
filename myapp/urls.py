1639ac3c191d2f4e87ab2ec2c892d573b7af7503
Edward-Dao <128796396+Edward-Dao@users.noreply.github.com>
2023-07-27 16:24:39 +0700

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('dashboard/', views.dashboard, name='dashboard'),
+
+    path('lists/', views.lists, name='lists'),
+    path('receipts/', views.receipts, name='receipts'),
+    path('reports/', views.reports, name='reports'),
 ]
