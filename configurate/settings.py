d93fe17c2c938df6b6f1c69cf86501f6285d7669
Edward-Dao <128796396+Edward-Dao@users.noreply.github.com>
2023-07-29 21:10:02 +0700

    Update settings.py

--- configurate/settings.py
+++ configurate/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'Agency_Management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'QLCDL/static'
+]
