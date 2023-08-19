66f4ad72d07cb59964caa80421f0bea6e20127f5
Kakalot215 <128162523+Kakalot215@users.noreply.github.com>
2023-08-19 20:43:20 +0700

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,12 +2,51 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login, name="login"),
-    path("login.html", views.login, name="login"),
-    path("register.html", views.register, name="register"),
-    path("dashboard.html", views.dashboard, name="mainpage"),
+    path("", views.webpage1, name="home"),
+    path("register/", views.webpage2, name="register"),
+    # URL for lists
+    path("daily/", views.DailyView.as_view(), name="daily_page"),
+    path("loaidaily/", views.LoaidailyView.as_view(), name="loaidaily_page"),
+    path("quan/", views.QuanView.as_view(), name="quan_page"),
+    path("mathang/", views.MathangView.as_view(), name="mathang_page"),
+    path("dvt/", views.DVTView.as_view(), name="dvt_page"),
 
-    path('lists/', views.lists, name='lists'),
-    path('receipts/', views.receipts, name='receipts'),
-    path('reports/', views.reports, name='reports'),
+    # URL for manipulating QUAN
+    path("quan/create", views.CreateQuan.as_view(), name="quan_create"),
+    path("quan/delete", views.RemoveQuan.as_view(), name="quan_remove"),
+    path("quan/update", views.UpdateQuan.as_view(), name="quan_update"),
+
+    # URL for manipulating DVT
+    path("dvt/create", views.CreateDVT.as_view(), name="dvt_create"),
+    path("dvt/delete", views.RemoveDVT.as_view(), name="dvt_remove"),
+    path("dvt/update", views.UpdateDVT.as_view(), name="dvt_update"),
+
+    # URL for manipulating LDL
+    path("loaidaily/create", views.CreateLoaidaily.as_view(), name="ldl_create"),
+    path("loaidaily/delete", views.RemoveLoaidaily.as_view(), name="ldl_remove"),
+    path("loaidaily/update", views.UpdateLoaidaily.as_view(), name="ldl_update"),
+    
+    # URL for manipulating MATHANG
+    path("mathang/create", views.CreateMathang.as_view(), name="mathang_create"),
+    path("mathang/remove", views.RemoveMathang.as_view(), name="mathang_remove"),
+    path("mathang/update", views.UpdateMathang.as_view(), name="mathang_update"),
+
+    # URL for bills
+    path("pnh/", views.PNHView.as_view(), name="pnh_page"),
+    path("pxh/", views.PXHView.as_view(), name="pxh_page"),
+    path("pctnh/", views.PCTNHView.as_view(), name="pctnh_page"),
+    path("pctxh/", views.PCTXHView.as_view(), name="pctxh_page"),
+    path("ptt/", views.PTTView.as_view(), name="ptt_page"),
+
+    # URL for reports
+    path("bccn/", views.BCCNView, name="bccn_page"),
+    path("bcds/", views.BCDSView.as_view(), name="bcds_page"),
+
+    # URL for manipulating BCCN
+    path("bccn/create", views.CreateBCCN.as_view(), name="bccn_create"),
+    path("bccn/delete", views.RemoveBCCN.as_view(), name="bccn_remove"),
+    path("bccn/update", views.UpdateBCCN.as_view(), name="bccn_update"),
+
+    # URL for regulations
+    path("regulations/", views.REGView.as_view(), name="regulations_page"),
 ]
